8f326a1652f56c0ce1a1305346b456e8801ebd4e
Anssi-Ruotsalainen <anssi.ruotsalainen@centria.fi>
2021-03-27 11:37:24 +0200

    27.3.2021

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -7,7 +7,45 @@ namespace exercise_30
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give me your percent [0 - 100]:");
+      int answer = Convert.ToInt32(Console.ReadLine());
 
+            if (answer < 0)
+	{
+                Console.WriteLine("impossible");
+	}
+            else if (answer <= 49)
+	{
+                Console.WriteLine("Fail");
+	}
+            else if (answer <= 59)
+	{
+                Console.WriteLine("Grade is: 1");
+	}
+            else if (answer <= 69)
+	{
+                Console.WriteLine("Grade is: 2");
+	}
+            else if (answer <= 79)
+	{
+                Console.WriteLine("Grade is: 3");
+	}
+            else if (answer <= 89)
+	{
+                Console.WriteLine("Grage is: 4");
+	}
+            else if (answer <= 100)
+	{
+                Console.WriteLine("Grade is: 5");
+	}
+            else
+	{
+              Console.WriteLine("Outstanding!");
+	}
+
+     Console.ReadKey();
     }
+
+   
   }
 }
