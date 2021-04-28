eef966f87da2ae980b3be19b4b4c2786f9fdf07c
Anssi Ruotsalainen <anssi.ruotsalainen@centria.fi>
2021-04-28 11:49:45 +0300

    28.4.2021

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,75 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
+            
+            ChristmasTree(10);
+            PrintSpaces(8);
+            PrintStars(3);
+            PrintSpaces(8);
+            PrintStars(3);
+
+            Console.ReadKey();
 
     }
 
     public static void PrintStars(int number)
     {
+             for (int i = 0; i < number; i++)
+			 { 
+                Console.Write("*"); 
+			 }
+             Console.WriteLine(" ");
 
     }
 
-    public static void PrintSpaces(int number)
+    public static void PrintSpaces(int space)
     {
-
+             for (int i = 0; i < space; i++)
+			{
+                Console.Write(" "); 
+			}
+            
     }
 
     public static void PrintRightTriangle(int size)
     {
+            for (int i = 1; i <= size; i++)
+			{
+                for (int j = 1; j <= size - i; j++)
+			    {
+                    Console.Write(" ");
+			    }
+                for (int k = 1; k <= i; k++)
+			    {
+                    Console.Write("*");
+
+			    }
+                Console.WriteLine(" ");
+
+			}
 
     }
 
     public static void ChristmasTree(int height)
     {
+         for (int i = 1; i <= height; i++)
+	     {
+               for (int j = 1; j <= (height - i); j++)
+               {
+                    Console.Write(" ");
+                  
+               } 
 
+               for (int t = 1; t < i * 2; t++)
+               {
+                    Console.Write("*");
+
+               }
+	           Console.WriteLine(" ");
+
+		 }
+           
+            
     }
   }
 }
